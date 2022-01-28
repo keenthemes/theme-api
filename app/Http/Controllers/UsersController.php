e5a1d4e69bb034cd80d7a65910af06703d6c1831
laurisstepanovs <la.stepanovs@gmailc.om>
2022-01-28 06:15:58 +0200

    feat: swagger fixes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Str;
 
 /**
  * @OA\Info(
